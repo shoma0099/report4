4c39dca9936bd2d6ddcd2f091ddfca2e74749c29
Shoma Nisimura <e195737@ie.u-ryukyu.ac.jp>
2019-12-09 03:56:50 +0900

    step3

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,13 +4,15 @@ public class Main {
 
     public static void main(String[] args) {
         // write your code here
-        try {
-            String str = null;
-            System.out.println(str.length());
-        }catch (NullPointerException e) {
-            System.out.println("例外が発生しました");
-            System.out.println(e.getMessage());
-        }
+        //try {
+        //    String str = null;
+        //    System.out.println(str.length());
+        //}catch (NullPointerException e) {
+        //    System.out.println("例外が発生しました");
+        //    System.out.println(e.getMessage());
+        //}
+        String str = "百二十三";
+        int value = Integer.parseInt(str);
 
     }
 }
