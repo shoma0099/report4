3bd0d13a7369527ccc582687afe468c14843bf6e
Shoma Nisimura <e195737@ie.u-ryukyu.ac.jp>
2019-12-09 03:20:44 +0900

    step1

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,7 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // write your code here
         String str = null;
         System.out.println(str.length());
     }
