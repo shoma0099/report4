882e3659ea6d524ab98355f8015f14c3b3c9a1ef
Shoma Nisimura <e195737@ie.u-ryukyu.ac.jp>
2019-12-09 02:57:48 +0900

    step1

---
+++ src/com/company/Main.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        String str = null;
+        System.out.println(str.length());
+    }
+}
